84116594c2a497263dd60a791a04c4b3c1dd255c
BGM <bgmp00@protonmail.com>
2019-03-27 01:03:24 -0300

    Cast player chat event

--- src/main/java/com/bgmbox/medievo/util/EventRegistry.java
+++ src/main/java/com/bgmbox/medievo/util/EventRegistry.java
@@ -1,9 +1,11 @@
 package com.bgmbox.medievo.util;
 
 import static com.bgmbox.medievo.EventHandlers.PlayerJoin.registerPlayerJoinEvent;
+import static com.bgmbox.medievo.EventHandlers.playerChat.registerPlayerChatEvent;
 
 public class EventRegistry {
     public static void registerEvents() {
         registerPlayerJoinEvent();
+        registerPlayerChatEvent();
     }
 }
