503e63c477d81acc11955c076791e36d38625db1
BGM <bgmp00@protonmail.com>
2019-03-28 18:36:44 -0300

    Clean unused imports

--- src/main/java/com/bgmbox/medievo/Ranks/DeliverRanks.java
+++ src/main/java/com/bgmbox/medievo/Ranks/DeliverRanks.java
@@ -1,7 +1,6 @@
 package com.bgmbox.medievo.Ranks;
 
 import com.bgmbox.medievo.Medievo;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.jdom2.Document;
