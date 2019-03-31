472a0bf1b0c198982a4ad39785d9cfebce02ae0c
BGM <bgmp00@protonmail.com>
2019-03-31 20:57:05 -0300

    Create a getter for clan leaders

---
+++ src/main/java/com/bgmbox/medievo/Database/Getters/ClanLeaderGetter.java
@@ -0,0 +1,29 @@
+package com.bgmbox.medievo.Database.Getters;
+
+import com.bgmbox.medievo.Medievo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClanLeaderGetter {
+
+    private static Medievo plugin = Medievo.getPlugin(Medievo.class);
+
+    public static String getClanLeaderUUID(String clanName) {
+        try {
+            return lookupClanLeaderUUID(clanName);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String lookupClanLeaderUUID(String clanName) throws SQLException {
+        PreparedStatement statement = plugin.getConnection().prepareStatement("SELECT * FROM " + plugin.clans_data_table + " WHERE clan=?");
+        statement.setString(1, clanName);
+        ResultSet results = statement.executeQuery();
+        results.next();
+        return results.getString("leader_uuid");
+    }
+}
