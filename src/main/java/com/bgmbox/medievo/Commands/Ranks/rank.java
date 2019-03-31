9f7f2baa27e97664ff803ed3be2e932bd413a98e
BGM <bgmp00@protonmail.com>
2019-03-31 18:54:25 -0300

    Add a color to rank references

--- src/main/java/com/bgmbox/medievo/Commands/Ranks/rank.java
+++ src/main/java/com/bgmbox/medievo/Commands/Ranks/rank.java
@@ -31,7 +31,7 @@ public class rank implements CommandExecutor {
             if (args.length == 3) {
                 String givenPlayerName = args[1];
                 String givenRank = args[2];
-                if (playerIsOnline(givenPlayerName)) {
+                if (playerIsOnline(givenPlayerName, false)) {
                     Player receiver = Bukkit.getServer().getPlayer(args[1]);
                     if (rankExists(givenRank)) {
                         String giver;
@@ -45,18 +45,18 @@ public class rank implements CommandExecutor {
                             case "add":
                                 addRank(receiver, givenRank);
                                 sender.sendMessage(ChatColor.GREEN + "Rank successfully added");
-                                receiver.sendMessage(ChatColor.GREEN + "You have been granted the " + ChatColor.WHITE + givenRank + ChatColor.GREEN + " rank by: " + ChatColor.RESET + giver);
+                                receiver.sendMessage(ChatColor.GREEN + "You have been granted the " + ChatColor.AQUA + givenRank + ChatColor.GREEN + " rank by: " + ChatColor.RESET + giver);
                                 break;
                             case "remove":
-                                sender.sendMessage(ChatColor.RED + "Rank successfully removed");
-                                receiver.sendMessage(ChatColor.RED + "You have been demoted from " + ChatColor.WHITE + givenRank + ChatColor.RED + " by: " + ChatColor.RESET + giver);
                                 removeRank(receiver, givenRank);
+                                sender.sendMessage(ChatColor.RED + "Rank successfully removed");
+                                receiver.sendMessage(ChatColor.RED + "You have been demoted from " + ChatColor.AQUA + givenRank + ChatColor.RED + " by: " + ChatColor.RESET + giver);
                                 break;
                             default:
                                 sender.sendMessage(GENERIC_SYNTAX_ERROR);
                         }
                     } else {
-                        sender.sendMessage(WARNING_ICON + ChatColor.WHITE + givenRank + ChatColor.RED + " is not a rank.");
+                        sender.sendMessage(WARNING_ICON + ChatColor.AQUA + givenRank + ChatColor.RED + " is not a rank.");
                     }
                 } else {
                     sender.sendMessage(WARNING_ICON + ChatColor.DARK_AQUA + givenPlayerName + ChatColor.RED + " is currently Offline or is not a valid player.");
