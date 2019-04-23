e3cf63e25fb5b5a61a91e62b09127c30a1049b75
BGM <bgmp00@protonmail.com>
2019-04-23 00:18:39 -0300

    Add extra information to clan sub-commands

--- src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/displayClansList.java
+++ src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/displayClansList.java
@@ -6,7 +6,11 @@ import org.bukkit.entity.Player;
 import java.util.List;
 import java.util.Objects;
 
+import static org.elmedievo.medievo.Commands.Clans.Mercantilism.Valuables.CURRENCY_SYMBOL;
+import static org.elmedievo.medievo.Database.Getters.ClanAlfonsosGetter.getClanAlfonsos;
+import static org.elmedievo.medievo.Database.Getters.ClanLeaderGetter.getClanLeaderName;
 import static org.elmedievo.medievo.Database.Getters.ClansToListGetter.getClansList;
+import static org.elmedievo.medievo.util.Methods.PlayerIsOnline.evalOnlinePlayer;
 
 public class displayClansList {
     public static void sendClansListToPlayer(Player player) {
@@ -23,7 +27,10 @@ public class displayClansList {
         StringBuilder clansListMessage = new StringBuilder();
 
         clansListMessage.append(ChatColor.RED + "##########" + ChatColor.AQUA + ChatColor.BOLD + " CLANS " + ChatColor.RED + "##########" + "\n");
-        Objects.requireNonNull(clans).forEach(clan -> clansListMessage.append(ChatColor.RED + "- " + ChatColor.GOLD).append(clan).append("\n"));
+        Objects.requireNonNull(clans).forEach(clan -> clansListMessage
+                .append(ChatColor.RED + "- " + ChatColor.AQUA + ChatColor.BOLD).append(clan)
+                .append(ChatColor.GRAY).append( " Leader: ").append(evalOnlinePlayer(getClanLeaderName(clan)))
+                .append(ChatColor.GRAY).append(" Balance: ").append(ChatColor.GOLD).append(CURRENCY_SYMBOL).append(getClanAlfonsos(clan)));
         clansListMessage.append(ChatColor.RED + "###########################");
 
         String finalClansListMessage = clansListMessage.toString();
--- src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/displayMembersList.java
+++ src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/displayMembersList.java
@@ -6,13 +6,14 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.elmedievo.medievo.Database.Getters.ClanMembersToList.getClanMembers;
+import static org.elmedievo.medievo.util.Methods.PlayerIsOnline.evalOnlinePlayer;
 
 public class displayMembersList {
     public static String displayClanMemberList(String clan) {
         List<String> members = getClanMembers(clan);
 
         StringBuilder memberListMessage = new StringBuilder();
-        Objects.requireNonNull(members).forEach(member -> memberListMessage.append(ChatColor.GRAY + "- " + ChatColor.YELLOW).append(member).append("\n"));
+        Objects.requireNonNull(members).forEach(member -> memberListMessage.append(ChatColor.GRAY + "- ").append(evalOnlinePlayer(member)).append("\n"));
 
         return memberListMessage.toString();
     }
