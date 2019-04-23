060286ec95591a991117c03a909ac1b9f4b2f81c
BGM <bgmp00@protonmail.com>
2019-04-23 00:17:57 -0300

    Implement an Eval for online-offline contemplation

--- src/main/java/org/elmedievo/medievo/util/Methods/PlayerIsOnline.java
+++ src/main/java/org/elmedievo/medievo/util/Methods/PlayerIsOnline.java
@@ -1,11 +1,21 @@
 package org.elmedievo.medievo.util.Methods;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class PlayerIsOnline {
     public static boolean playerIsOnline(String name) {
         Player player = Bukkit.getServer().getPlayer(name);
         return player != null;
+    }
+
+    public static String evalOnlinePlayer(String name) {
+        if (playerIsOnline(name)) {
+            Player player = Bukkit.getServer().getPlayer(name);
+            return player.getDisplayName();
+        } else {
+            return ChatColor.DARK_AQUA + name;
+        }
     }
 }
