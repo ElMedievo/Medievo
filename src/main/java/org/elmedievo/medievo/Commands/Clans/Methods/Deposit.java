9c691f064d3c0e98a86d76deed746a9d7769523a
BGM <bgmp00@protonmail.com>
2019-04-27 21:22:48 -0300

    Display deposit message to every clan member

--- src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/Deposit.java
+++ src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/Deposit.java
@@ -1,21 +1,25 @@
 package org.elmedievo.medievo.Commands.Clans.Methods;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
 import java.util.Objects;
 
 import static org.elmedievo.medievo.Commands.Clans.Mercantilism.Valuables.CURRENCY_NAME_PLURAL;
 import static org.elmedievo.medievo.Commands.Clans.Mercantilism.Valuables.CURRENCY_SYMBOL;
 import static org.elmedievo.medievo.Commands.Clans.Mercantilism.Valuables.valueInMarket;
+import static org.elmedievo.medievo.Database.Getters.ClanMembersToList.getClanMembers;
 import static org.elmedievo.medievo.Database.Getters.PlayerClanGetter.getPlayerClan;
 import static org.elmedievo.medievo.Database.Setters.AddAlfonsosToClan.addAlfonsosToClan;
 import static org.elmedievo.medievo.Database.Setters.AddGoldToClan.addGoldToClan;
 import static org.elmedievo.medievo.util.Generic.CANNOT_DEPOSIT;
 import static org.elmedievo.medievo.util.Generic.DEPOSIT_SUCCESS;
 import static org.elmedievo.medievo.util.Generic.NOT_IN_A_CLAN;
+import static org.elmedievo.medievo.util.Methods.PlayerIsOnline.playerIsOnline;
 
 public class Deposit {
     public static void depositGoldIntoClan(Player player) {
@@ -31,7 +35,14 @@ public class Deposit {
                 player.setItemInHand(new ItemStack(Material.AIR));
                 addAlfonsosToClan(playerClan, alfonsos);
                 addGoldToClan(playerClan, material, materialAmount);
-                player.sendMessage(DEPOSIT_SUCCESS + ChatColor.AQUA + " » " + ChatColor.WHITE + ChatColor.UNDERLINE + ChatColor.ITALIC + CURRENCY_SYMBOL + alfonsos + " " + CURRENCY_NAME_PLURAL);
+                String clan = getPlayerClan(player.getUniqueId());
+                List<String> clanMembers = getClanMembers(clan);
+                Objects.requireNonNull(clanMembers).forEach(member -> {
+                    if (playerIsOnline(member)) {
+                        Player member_player = Bukkit.getServer().getPlayer(member);
+                        member_player.sendMessage(member_player.getDisplayName()  + ChatColor.AQUA + " » " + DEPOSIT_SUCCESS + ChatColor.AQUA + " » " + ChatColor.WHITE + ChatColor.UNDERLINE + ChatColor.ITALIC + CURRENCY_SYMBOL + alfonsos + " " + CURRENCY_NAME_PLURAL);
+                    }
+                });
             }
         } else {
             player.sendMessage(NOT_IN_A_CLAN);
