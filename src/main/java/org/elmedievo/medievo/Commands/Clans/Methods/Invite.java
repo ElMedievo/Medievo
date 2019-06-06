3a7f55d5b94266b8647ee6cff91b1e2037d4348e
BGM <bgmp00@protonmail.com>
2019-06-06 08:13:59 -0400

    Prevent clan invitations to "none"

--- src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/Invite.java
+++ src/main/java/org/elmedievo/medievo/Commands/Clans/Methods/Invite.java
@@ -14,6 +14,7 @@ import static org.elmedievo.medievo.util.Generic.WARNING_ICON;
 import static org.elmedievo.medievo.util.Methods.PlayerIsOnline.playerIsOnline;
 
 public class Invite {
+    @SuppressWarnings("deprecation")
     public static void createPlayerToPlayerInvite(String inviter_name, String invited_name) {
         Player inviter_player = Bukkit.getPlayer(inviter_name);
         String inviters_clan = getPlayerClan(inviter_player.getUniqueId());
@@ -21,15 +22,20 @@ public class Invite {
             Player invited_player = Bukkit.getPlayer(invited_name);
             String inviteds_clan = getPlayerClan(invited_player.getUniqueId());
             if (Objects.requireNonNull(inviteds_clan).equals("none")) {
-                if (!isAlreadyBeingInvited(invited_name)) {
-                    inviteQueue.put(invited_name, inviter_name);
-                    invited_player.sendMessage(
-                            ChatColor.GREEN + "You have been invited to join " + ChatColor.AQUA + inviters_clan + ChatColor.GREEN + " by: " + inviter_player.getDisplayName() + ChatColor.GREEN + "!" + "\n"
-                                    + ChatColor.GREEN + "Use " + ChatColor.AQUA + "/accept" + ChatColor.GREEN + " to join or " + ChatColor.AQUA + "/decline " + ChatColor.GREEN + " to dismiss this invitation"
-                    );
-                    inviter_player.sendMessage(ChatColor.GREEN + "Invitation successfully sent to " + invited_player.getDisplayName());
+                if (!Objects.requireNonNull(inviters_clan).equals("none")) {
+                    if (!isAlreadyBeingInvited(invited_name)) {
+                        inviteQueue.put(invited_name, inviter_name);
+                        invited_player.sendMessage(
+                                ChatColor.GREEN + "You have been invited to join " + ChatColor.AQUA + inviters_clan + ChatColor.GREEN + " by: " + inviter_player.getDisplayName() + ChatColor.GREEN + "!" + "\n"
+                                        + ChatColor.GREEN + "Use " + ChatColor.AQUA + "/accept" + ChatColor.GREEN + " to join or " + ChatColor.AQUA + "/decline " + ChatColor.GREEN + " to dismiss this invitation"
+                        );
+                        inviter_player.sendMessage(ChatColor.GREEN + "Invitation successfully sent to " + invited_player.getDisplayName());
                 } else {
-                    inviter_player.sendMessage(WARNING_ICON + invited_player.getDisplayName() + ChatColor.RED + " is already invited to join a clan! Tell them to " + ChatColor.YELLOW + "/decline " + ChatColor.RED + "their invitation!");
+                        inviter_player.sendMessage(WARNING_ICON + invited_player.getDisplayName() + ChatColor.RED + " is already invited to join a clan! Tell them to " + ChatColor.YELLOW + "/decline " + ChatColor.RED + "their invitation!");
+
+                    }
+                } else {
+                    inviter_player.sendMessage(WARNING_ICON + ChatColor.RED + "You are not in a clan!");
                 }
             } else {
                 inviter_player.sendMessage(WARNING_ICON + invited_player.getDisplayName() + ChatColor.RED + " is already in a clan!");
